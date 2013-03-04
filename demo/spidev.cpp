ee2ba0605d20e117940920f7f3b19dfde83e2451
Phillip Peach <pjp2@sfu.ca>
2013-03-03 17:33:05 -0800

    Fix to use acutal values for the RPI and MCP4812

--- demo/spidev.cpp
+++ demo/spidev.cpp
@@ -1,9 +1,10 @@
 #include "../lib/es_SPIDEV.hpp"
 
 int main(int argc, char *argv[]){
-    es_SPIDEV interface = es_SPIDEV("/dev/spidev0.0");
-    unsigned char tx_buf[1];
-    tx_buf[0] = 0xFF;
+    es_SPIDEV interface = es_SPIDEV("/dev/spidev0.1");
+    unsigned char tx_buf[2];
+    tx_buf[0] = 0x1F;
+    tx_buf[1] = 0xFF;
     interface.transmit(tx_buf, true);
     return 0;
 }
