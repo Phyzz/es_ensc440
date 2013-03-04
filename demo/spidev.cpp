db059eb08131d3444339f03f6d7e378325fd95ea
Phillip Peach <pjp2@sfu.ca>
2013-03-03 21:58:57 -0800

    Uncommented lines in the demo

--- demo/spidev.cpp
+++ demo/spidev.cpp
@@ -7,16 +7,16 @@ int main(int argc, char *argv[]){
     es_ADC adc = es_ADC("/dev/spidev0.0");
     dac.setChannelLevel(CH_A, 0x3FF, false, false);
     dac.setChannelLevel(CH_B, 0x3FF, false, false);
-//    int ch0 = es_ADC.takeReading(CH_0, false);
-//    int ch1 = es_ADC.takeReading(CH_1, false);
-//    std::cout << "CH_0 reading: " << std::hex << ch0 << "\n";
-//    std::cout << "CH_1 reading: " << std::hex << ch1 << "\n";
+    int ch0 = adc.takeReading(CH_0, false);
+    int ch1 = adc.takeReading(CH_1, false);
+    std::cout << "CH_0 reading: " << std::hex << ch0 << "\n";
+    std::cout << "CH_1 reading: " << std::hex << ch1 << "\n";
     sleep(5);
     dac.setChannelLevel(CH_A, 0x000, false, false);
     dac.setChannelLevel(CH_B, 0x000, false, false);
-//    ch0 = es_ADC.takeReading(CH_0, false);
-//    ch1 = es_ADC.takeReading(CH_1, false);
-//    std::cout << "CH_0 reading: " << std::hex << ch0 << "\n";
-//    std::cout << "CH_1 reading: " << std::hex << ch1 << "\n";
+    ch0 = adc.takeReading(CH_0, false);
+    ch1 = adc.takeReading(CH_1, false);
+    std::cout << "CH_0 reading: " << std::hex << ch0 << "\n";
+    std::cout << "CH_1 reading: " << std::hex << ch1 << "\n";
     return 0;
 }
