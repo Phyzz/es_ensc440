bbed7afd7e2a958095daf4360485e9f87d40b2e0
Phillip Peach <pjp2@sfu.ca>
2013-03-04 18:50:09 -0800

    Fixed some mistakes in the bit bashing. Don't code while tired :)

--- lib/es_ADC.cpp
+++ lib/es_ADC.cpp
@@ -1,5 +1,5 @@
 #include "es_ADC.hpp"
-#include "es_EX.hpp"
+#include "es_SPI_EX.hpp"
 
 es_ADC::es_ADC(string device_path) : interface(device_path) {
 }
@@ -7,18 +7,18 @@ es_ADC::es_ADC(string device_path) : interface(device_path) {
 int es_ADC::takeReading(ADCChannelEnum channel, bool differential_reading) {
     unsigned char tx_buffer[1];
     if (differential_reading) {
-        tx_buffer[0] = 0x10;
+        tx_buffer[0] = 0x20;
     } else {
-        tx_buffer[0] = 0x18;
+        tx_buffer[0] = 0x30;
     }
     if (channel == CH_0) {
         tx_buffer[0] |= 0x00;
     } else if (channel == CH_1) {
-        tx_buffer[0] |= 0x01;
-    } else if (channel == CH_2) {
         tx_buffer[0] |= 0x02;
+    } else if (channel == CH_2) {
+        tx_buffer[0] |= 0x04;
     } else if (channel == CH_3) {
-        tx_buffer[0] |= 0x03;
+        tx_buffer[0] |= 0x06;
     }
     
     unsigned char rx_buffer[2];
@@ -26,12 +26,13 @@ int es_ADC::takeReading(ADCChannelEnum channel, bool differential_reading) {
     rx_buffer[1] = 0x00;
     
     this->interface.half_duplex(tx_buffer, 1, rx_buffer, 2, false, true);
+    if (rx_buffer[0] & 0x80) {
+        throw es_SPI_EX_TRANSFER();
+    }
     
-    int reading = 0;
-    reading |= rx_buffer[0];
-    reading &= 0x7F;
-    reading <<= 4;
-    rx_buffer[1] >>= 4;
+    int reading = rx_buffer[0];
+    reading <<= 3;
+    rx_buffer[1] >>= 5;
     reading |= rx_buffer[1];
     
     return reading;
