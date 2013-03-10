3f61fb6d552e7ccab72d029ee72cabe71ccebc7b
Phillip Peach <pjp2@sfu.ca>
2013-03-10 14:07:37 -0700

    Moved the time update which uped the max frequency to about 12.7kHz

--- arduino/demo/square/square.ino
+++ arduino/demo/square/square.ino
@@ -2,21 +2,25 @@
 
 int DAC_SS = 9;
 
-int PERIOD = 1000; //in microseconds, freq of 1kHz
+int PERIOD = 100; //in microseconds, freq of 10kHz
 boolean wave_state = false;
 unsigned long next_action_time = 0;
+
+//unsigned char[4] level_bytes = { 0x10, 0x00, 0x1F, 0xFF};
+//char i = 0;
 
 
 void setup() {
   SPI.setBitOrder(MSBFIRST);
   SPI.setDataMode(SPI_MODE0);
-  SPI.setClockDivider(SPI_CLOCK_DIV128);
+  SPI.setClockDivider(SPI_CLOCK_DIV2);
   pinMode(DAC_SS, OUTPUT);
   SPI.begin();
 }
 
 void loop() {
   if( micros() > next_action_time) {
+    next_action_time += (PERIOD/2);
     unsigned char level_bytes[2];
     if (wave_state) {
       level_bytes = {0x10, 0x00};
@@ -29,6 +33,5 @@ void loop() {
     }
     digitalWrite(DAC_SS, HIGH);
     wave_state = !wave_state;
-    next_action_time += (PERIOD/2);
   }
 }
