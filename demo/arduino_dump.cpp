571998fec84cf8ac4a8f58aea41c370ee91f7974
Phillip Peach <pjp2@sfu.ca>
2013-03-19 14:13:24 -0700

    Integretted FFT into the arduino dump demo

--- demo/arduino_dump.cpp
+++ demo/arduino_dump.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "../lib/es_SPI_EX.hpp"
 #include "../lib/es_SPIDEV.hpp"
+#include "../lib/ffft/FFTReal.h"
 
 int main(int argc, char *argv[]){
     unsigned char rx_buf[1032];
@@ -13,11 +14,33 @@ int main(int argc, char *argv[]){
         results[i] = (rx_buf[i*2 + 1] << 8);
         results[i] |= rx_buf[i*2 + 2];
     }
+    
     for (int i = 0; i < 512; i += 16) {
         for (int j = 0; j < 16 && i +j < 515; ++j) {
             std::cout << std::hex << results[i + j] << "\t";
         }
         std::cout << std::endl;
     }
+    
+    float samples[512];
+    for (int i = 0; i < 512; ++i) {
+        samples[i] = (float) results[i];
+    }
+    float fft_results[512];
+    ffft::FFTReal <float> fft_object (512);
+    fft_object.do_fft(fft_results, samples);
+    
+    int highest_index = 0;
+    float highest_result = 0.0;
+    for (int i = 1; i < 255; ++i) {
+        float result = results[i] * results[i] + results[256+i] * results[256*i];
+        if (result > highest_result) {
+            highest_result = result;
+            highest_index = i;
+        }
+    }
+    
+    std::cout << std::endl << "Highest frequency is " << 1.953125f * highest_index << std::endl;
+    
     return 0;
 }
